6d2ff72fb31b98bb0a066b064f8367c9ee1d4e69
Sandy <Somyajain2208@gmail.com>
2025-01-11 10:59:51 +0530

    header folder changed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Header.css';
 import Logo from '../../assets/Ambaincip_red.svg';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
@@ -13,52 +14,52 @@ const Header = () => {
           <span className='line'></span>
           <nav>
             <div className="dropdown">
-              <a href="/academics" className="dropdown-link">
+              <NavLink to="/academics" className="dropdown-link">
                 Academics &nbsp;<i className="fas fa-chevron-down"></i> &nbsp;/
-              </a>
+              </NavLink>
               <div className="dropdown-menu">
-                <a href="#undergraduate">Undergraduate</a>
-                <a href="#graduate">Graduate</a>
-                <a href="#schools">Schools</a>
-                <a href="/global-initiatives">Global Initiatives</a>
-                <a href="/interdisciplinary">Interdisciplinary</a>
-                <a href="/online-learning">Online Learning</a>
-                <a href="/off-campus-learning">Off-campus Learning</a>
-                <a href="#libraries">Libraries</a>
+                <NavLink to="/undergraduate">Undergraduate</NavLink>
+                <NavLink to="/graduate">Graduate</NavLink>
+                <NavLink to="/schools">Schools</NavLink>
+                <NavLink to="/global-initiatives">Global Initiatives</NavLink>
+                <NavLink to="/interdisciplinary">Interdisciplinary</NavLink>
+                <NavLink to="/online-learning">Online Learning</NavLink>
+                <NavLink to="/off-campus-learning">Off-campus Learning</NavLink>
+                <NavLink to="/libraries">Libraries</NavLink>
               </div>
             </div>
-            <a href="/admissions">Admissions & Aid &nbsp;/</a>
-            <a href="/athletics">Athletics &nbsp;/</a>
+            <NavLink to="/admissions">Admissions & Aid &nbsp;/</NavLink>
+            <NavLink to="/athletics">Athletics &nbsp;/</NavLink>
             <div className="dropdown">
-              <a href="/research-and-innovation" className="dropdown-link">
+              <NavLink to="/research-and-innovation" className="dropdown-link">
                 Research & Innovation &nbsp;<i className="fas fa-chevron-down"></i> &nbsp;/
-              </a>
+              </NavLink>
               <div className="dropdown-menu">
-                <a href="/research-enterprise">Research Enterprise</a>
-                <a href="/research-news">Research News</a>
+                <NavLink to="/research-enterprise">Research Enterprise</NavLink>
+                <NavLink to="/research-news">Research News</NavLink>
               </div>
             </div>
             <div className="dropdown">
-              <a href="#life" className="dropdown-link">
+              <NavLink to="/life" className="dropdown-link">
                 Life at Penn &nbsp;<i className="fas fa-chevron-down"></i> &nbsp;/
-              </a>
+              </NavLink>
               <div className="dropdown-menu">
-                <a href="/arts">Arts & Culture</a>
-                <a href="#diversity">Diversity</a>
-                <a href="/groups-and-organizations">Groups & Organizations</a>
-                <a href="/housing-and-dining">Housing & Dining</a>
-                <a href="/health-wellness">Health & Wellness</a>
+                <NavLink to="/arts">Arts & Culture</NavLink>
+                <NavLink to="/diversity">Diversity</NavLink>
+                <NavLink to="/groups-and-organizations">Groups & Organizations</NavLink>
+                <NavLink to="/housing-and-dining">Housing & Dining</NavLink>
+                <NavLink to="/health-wellness">Health & Wellness</NavLink>
               </div>
             </div>
             <div className="dropdown">
-              <a href="/about" className="dropdown-link">
+              <NavLink to="/about" className="dropdown-link">
                 About &nbsp;<i className="fas fa-chevron-down"></i>
-              </a>
+              </NavLink>
               <div className="dropdown-menu">
-                <a href="/faq">FAQ</a>
-                <a href="/policies">Policies</a>
-                <a href="/facts">Facts</a>
-                <a href="/university-values">University Values</a>
+                <NavLink to="/faq">FAQ</NavLink>
+                <NavLink to="/policies">Policies</NavLink>
+                <NavLink to="/facts">Facts</NavLink>
+                <NavLink to="/university-values">University Values</NavLink>
               </div>
             </div>
           </nav>
